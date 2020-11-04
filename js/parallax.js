44c23dc3f7974c776df560a9a94c66c6981b001c
ferranclua <38246057+ferranclua@users.noreply.github.com>
2020-11-04 09:54:09 +0100

    Delete parallax.js

--- js/parallax.js
+++
@@ -1,8 +0,0 @@
-let parallax = document.querySelector('.parallax');
-
-function scrollParallax() {
-    let scrollTop = document.documentElement.scrollTop;
-    parallax.style.transform = 'translateY(' + scrollTop * -0.1 + 'px)';
-}
-
-window.addEventListener('scroll', scrollParallax);
