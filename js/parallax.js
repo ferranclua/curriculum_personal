57dfb33dec375787467be467625e6dfdc8f3b049
ferranclua <38246057+ferranclua@users.noreply.github.com>
2020-11-11 21:57:06 +0100

    Delete parallax.js

--- js/parallax.js
+++
@@ -1,8 +0,0 @@
-let parallax = document.querySelector('.photo');
-
-function scrollParallax() {
-    let scrollTop = document.documentElement.scrollTop;
-    parallax.style.transform = 'translateY(' + scrollTop * -0.2 + 'px)';
-}
-
-window.addEventListener('scroll', scrollParallax);
